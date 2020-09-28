ab4d0b6bd3ad2ce486a85ba7afa6cfae1df035c0
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-09-28 14:45:46 +0900

    修正２

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
-  before_action :basic_auth
   def index
     @posts = Post.all.order(id: "DESC")
   end
 
   def create
-    Post.create(content: params[:content], checked: false)
+    post = Post.create(content: params[:content], checked: false)
     render json: { post: post }
   end
 
@@ -19,13 +18,5 @@ class PostsController < ApplicationController
 
     item = Post.find(params[:id])
     render json: { post: item }
-  end
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 end
